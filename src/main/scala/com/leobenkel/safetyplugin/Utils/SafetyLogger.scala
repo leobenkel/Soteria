5ec423e896d5e808e55bd44d9d09836dc1d47b49
Leo Benkel <lbenkel@demandbase.com>
2019-05-31 15:51:53 -0700

    Increase test for SafetyLogger

--- src/main/scala/com/leobenkel/safetyplugin/Utils/SafetyLogger.scala
+++ src/main/scala/com/leobenkel/safetyplugin/Utils/SafetyLogger.scala
@@ -8,15 +8,26 @@ case class SafetyLogger(
   level:     Level.Value,
   softError: Boolean
 ) extends sbt.util.Logger with LoggerExtended {
+
+  /**
+    * For test only
+    */
+  object TestOnly {
+    @inline def makeSeparatorTest(title: String): String = makeSeparator(title)
+    @transient lazy val getLevelTest: Level.Value = getLevel
+    @inline def atLevelTest(level:         Level.Value): Boolean = atLevel(level)
+    @inline def prependHeaderTest(message: => String):   String = prependHeader(message)
+  }
+
   innerLog.setLevel(Level.Debug)
 
-  def getLevel: Level.Value = this.level
+  @transient lazy private val getLevel: Level.Value = this.level
 
-  def setLevel(level: Level.Value): SafetyLogger = this.copy(level = level)
+  override def setLevel(level: Level.Value): SafetyLogger = this.copy(level = level)
 
-  def atLevel(level: Level.Value): Boolean = level.id >= getLevel.id
+  private def atLevel(level: Level.Value): Boolean = level.id >= getLevel.id
 
-  private def prependHeader(message: => String): String = s"[SafetyPlugin] $message"
+  private def prependHeader(message: => String): String = s"[${SafetyLogger.Header}] $message"
 
   override def trace(t: => Throwable): Unit = innerLog.trace(t)
 
@@ -31,7 +42,7 @@ case class SafetyLogger(
     }
   }
 
-  def fail(message: String): Unit = {
+  override def fail(message: String): Unit = {
     if (softError) {
       error(prependHeader(message))
     } else {
@@ -51,19 +62,29 @@ case class SafetyLogger(
     level: Level.Value,
     title: String
   ): Unit = {
-    val totalLineLength = 70
-    val LeftForEdge = Math.ceil((totalLineLength - title.length - 4) / 2).toInt
-    val edge = (0 until LeftForEdge).map(_ => "-").mkString("")
-    val fullMessage = s"$edge< $title >$edge"
-    val extra = if (fullMessage.length < totalLineLength) {
-      "-"
+    log(level, makeSeparator(title))
+  }
+
+  private def makeSeparator(title: String): String = {
+    val goodTitle = title.trim
+    val LeftForEdge = Math.ceil((SafetyLogger.SeparatorLength - goodTitle.length - 4) / 2).toInt
+    val edge = SafetyLogger.SeparatorCharacter * LeftForEdge
+    val fullMessage = s"$edge< $goodTitle >$edge"
+    val extra = if (fullMessage.length < SafetyLogger.SeparatorLength) {
+      SafetyLogger.SeparatorCharacter
     } else {
       ""
     }
-    log(level, fullMessage + extra)
+    fullMessage + extra
   }
 
   override def isSoftError: Boolean = this.softError
 
   override def setSoftError(softError: Boolean): SafetyLogger = this.copy(softError = softError)
+}
+
+object SafetyLogger {
+  val Header: String = "SafetyPlugin"
+  val SeparatorLength:    Int = 70
+  val SeparatorCharacter: String = "-"
 }
