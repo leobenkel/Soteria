06c1631d8b670da10dfade90331ec96cd7bb9de3
Leo Benkel <lbenkel@demandbase.com>
2019-06-03 16:21:08 -0700

    Reduce the size of the method 'checkVersion'
    https://github.com/leobenkel/safety_plugin/issues/15

--- src/main/scala/com/leobenkel/safetyplugin/Transformations/CheckVersion.scala
+++ src/main/scala/com/leobenkel/safetyplugin/Transformations/CheckVersion.scala
@@ -1,11 +1,11 @@
 package com.leobenkel.safetyplugin.Transformations
 
 import com.leobenkel.safetyplugin.Config.SafetyConfiguration
-import com.leobenkel.safetyplugin.Messages.CommonMessage.ResultMessages
 import com.leobenkel.safetyplugin.Messages.ErrorMessage
 import com.leobenkel.safetyplugin.Utils.LoggerExtended
 import com.leobenkel.safetyplugin.Utils.ImplicitModuleToString._
 import com.leobenkel.safetyplugin.Messages.CommonMessage._
+import com.leobenkel.safetyplugin.Modules.Dependency
 import sbt.ModuleID
 
 private[Transformations] trait CheckVersion {
@@ -16,9 +16,9 @@ private[Transformations] trait CheckVersion {
     * @param libraries the libraries fetch by the build, coming from [[sbt.Keys.allDependencies]]
     * @param log       The current log
     */
-   def checkVersion(
+  def checkVersion(
     log:        LoggerExtended,
-    config: SafetyConfiguration,
+    config:     SafetyConfiguration,
     libraries:  Seq[ModuleID],
     moreErrors: ErrorMessage = ErrorMessage.Empty
   ): ResultMessages = {
@@ -26,36 +26,59 @@ private[Transformations] trait CheckVersion {
 
     val allCorrectLibraries = config.CorrectVersions
 
-    val librariesToCheck: Seq[ModuleID] = for {
+    val librariesToCheck: Seq[ModuleID] = getLibraryToCheck(allCorrectLibraries, libraries)
+
+    log.debug(s"> Verifying version of libraries (${librariesToCheck.size}) :")
+    librariesToCheck.prettyString(log, "checkVersion")
+
+    consolidateErrors(allCorrectLibraries, librariesToCheck, moreErrors)
+  }
+
+  private def getLibraryToCheck(
+    allCorrectLibraries: Seq[Dependency],
+    libraries:           Seq[ModuleID]
+  ): Seq[ModuleID] = {
+    for {
       correctLibrary <- allCorrectLibraries
       library        <- libraries if correctLibrary === library
     } yield {
       library
     }
+  }
 
-    log.debug(s"> Verifying version of libraries (${librariesToCheck.size}) :")
-    librariesToCheck.prettyString(log, "checkVersion")
-
+  private def consolidateErrors(
+    allCorrectLibraries: Seq[Dependency],
+    librariesToCheck:    Seq[ModuleID],
+    moreErrors:          ErrorMessage
+  ): ResultMessages = {
     (allCorrectLibraries
       .filter(_.version.isRight)
       .map(m => (m, m.version.right.get))
       .flatMap {
         case (correctModule, correctVersion) =>
-          librariesToCheck
-            .filter(m => (correctModule === m) && m.revision != correctVersion)
-            .map { m =>
-              val correctModuleToString = m
-                .withRevision(correctVersion)
-                .withName(correctModule.name)
-                .prettyString
-
-              s"${m.prettyString} should be $correctModuleToString"
-            }
+          buildErrors(librariesToCheck, correctModule, correctVersion)
       }
       .toError("Wrong versions") ++ moreErrors)
       .resolve(
         s"> All ${librariesToCheck.size} libraries that we " +
           s"know have the right versions"
       )
+  }
+
+  private def buildErrors(
+    librariesToCheck: Seq[ModuleID],
+    correctModule:    Dependency,
+    correctVersion:   String
+  ): Seq[String] = {
+    librariesToCheck
+      .filter(m => (correctModule === m) && m.revision != correctVersion)
+      .map { m =>
+        val correctModuleToString = m
+          .withRevision(correctVersion)
+          .withName(correctModule.name)
+          .prettyString
+
+        s"${m.prettyString} should be $correctModuleToString"
+      }
   }
 }
