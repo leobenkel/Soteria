6e2ab8488ac92d8d3e2655a3e9eccb94e169d362
Leo Benkel <lbenkel@demandbase.com>
2019-12-06 11:56:44 -0800

    Fix issue #37

---
+++ src/main/scala/com/leobenkel/safetyplugin/Transformations/AddScalaFixCompilerPlugin.scala
@@ -0,0 +1,27 @@
+package com.leobenkel.safetyplugin.Transformations
+
+import sbt._
+import scala.util.matching.Regex
+
+trait AddScalaFixCompilerPlugin {
+
+  private[Transformations] def shouldAddCompilerPlugin(scalaVersion: String): Boolean = {
+    val pattern: Regex = "^2\\.1[012]\\..*$".r
+    pattern.pattern.matcher(scalaVersion).find()
+  }
+
+  def addScalaFixCompilerPlugin(): Def.Initialize[Seq[ModuleID]] = {
+    Def.settingDyn {
+      val scalaVersion = Keys.scalaVersion.value
+      val libraries = Keys.libraryDependencies.value
+
+      Def.setting {
+        if (shouldAddCompilerPlugin(scalaVersion)) {
+          libraries :+ compilerPlugin(scalafix.sbt.ScalafixPlugin.autoImport.scalafixSemanticdb)
+        } else {
+          libraries
+        }
+      }
+    }
+  }
+}
