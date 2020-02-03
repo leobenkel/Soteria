2b1bdef499cb4e147795d57b224b63f645445450
Leo Benkel <lbenkel@demandbase.com>
2020-02-03 11:50:04 -0800

    Add shortcut commands for coveralls
    * safetyRunTestCoverage
    * safetyRunSubmitCoverage

--- src/main/scala/com/leobenkel/safetyplugin/SafetyPluginKeys.scala
+++ src/main/scala/com/leobenkel/safetyplugin/SafetyPluginKeys.scala
@@ -4,49 +4,65 @@ import com.leobenkel.safetyplugin.Config.SafetyConfiguration
 import com.leobenkel.safetyplugin.Utils.SafetyLogger
 import sbt.librarymanagement.ModuleID
 import sbt.util.Level
-import sbt.{settingKey, taskKey}
+import sbt.{TaskKey, settingKey, taskKey}
 import sbtassembly.AssemblyOption
 
 private[safetyplugin] object SafetyPluginKeys {
+  case class FancySettings[A](
+    setting:      A,
+    nameAsString: String
+  )
+
   // Upper case SettingKey and TaskKey does not work.
   // Use settingKey and taskKey.
 
-  val safetyGetLog = settingKey[SafetyLogger]("private")
+  lazy val safetyGetLog = settingKey[SafetyLogger]("private")
 
-  val safetyConfPath = settingKey[String]("Path to the configuration file.")
-  val safetyConfig = settingKey[SafetyConfiguration]("private")
-  val safetyAssemblySettings = taskKey[AssemblyOption](
+  lazy val safetyConfPath = settingKey[String]("Path to the configuration file.")
+  lazy val safetyConfig = settingKey[SafetyConfiguration]("private")
+  lazy val safetyAssemblySettings = taskKey[AssemblyOption](
     "Use to set the Assemble option to the right values"
   )
 
-  val safetyGetAllDependencies = settingKey[Seq[ModuleID]](
+  lazy val safetyGetAllDependencies = settingKey[Seq[ModuleID]](
     "Will return the list of all the dependencies known."
   )
 
-  val defaultAssemblyOption = taskKey[AssemblyOption]("private")
+  lazy val defaultAssemblyOption = taskKey[AssemblyOption]("private")
 
-  val safetySoftOnCompilerWarning = settingKey[Boolean](
+  lazy val safetySoftOnCompilerWarning = settingKey[Boolean](
     "If true, will not fail compilation on compiler warning."
   )
 
-  val safetyBuildConfig = taskKey[SafetyConfiguration]("Used for command. Do not call")
+  lazy val safetyBuildConfig = taskKey[SafetyConfiguration]("Used for command. Do not call")
 
-  val safetyDebugModule = settingKey[Option[(String, String)]](
+  lazy val safetyDebugModule = settingKey[Option[(String, String)]](
     "When set, will print out the dependency of this module."
   )
 
-  val safetySoft = settingKey[Boolean](
+  lazy val safetySoft = settingKey[Boolean](
     "If true, won't fail compilation but throw warnings."
   )
 
-  val safetyLogLevel = settingKey[Level.Value](
+  lazy val safetyLogLevel = settingKey[Level.Value](
     "Set the level of logs for the safety Plugin."
   )
 
   // Scala style
-  val safetyCheckScalaStyle = taskKey[Unit]("Run ScalaStyle.")
-  val safetyCheckScalaFix = taskKey[Unit]("Run ScalaFix.")
-  val safetyCheckScalaFmt = taskKey[Boolean]("Run ScalaFmtCheck.")
-  val safetyCheckScalaCheckAll = taskKey[Unit]("Check all scala style.")
-  val safetyCheckScalaFmtRun = taskKey[Unit]("Run ScalaFmt.")
+  lazy val safetyCheckScalaStyle = taskKey[Unit]("Run ScalaStyle.")
+  lazy val safetyCheckScalaFix = taskKey[Unit]("Run ScalaFix.")
+  lazy val safetyCheckScalaFmt = taskKey[Boolean]("Run ScalaFmtCheck.")
+  lazy val safetyCheckScalaCheckAll = taskKey[Unit]("Check all scala style.")
+  lazy val safetyCheckScalaFmtRun = taskKey[Unit]("Run ScalaFmt.")
+
+  // coverall
+  lazy val safetyTestCoverage: FancySettings[TaskKey[Unit]] = FancySettings(
+    setting = taskKey[Unit]("PRIVATE - Run test coverage"),
+    nameAsString = "safetyTestCoverage"
+  )
+  lazy val safetySubmitCoverage: FancySettings[TaskKey[Unit]] = FancySettings(
+    setting = taskKey[Unit]("PRIVATE - Run test coverage and submit coverage"),
+    nameAsString = "safetySubmitCoverage"
+  )
+  lazy val safetyCheckCoverallEnvVar = taskKey[Unit]("PRIVATE")
 }
